fa47e8edf353c6479d25e99695a44cb9ea015947
Sodiq Alabi <adisco4420@gmail.com>
2023-05-23 22:49:13 +0100

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   sideMenuItems = [
     {icon: 'fab fa-airbnb', label: 'All Homes'},
-    {icon: 'fab fa-airbnb', label: 'All Homes'},
-    {icon: 'fab fa-airbnb', label: 'All Homes'},
-    {icon: 'fab fa-airbnb', label: 'All Homes'},
-    {icon: 'fab fa-airbnb', label: 'All Homes'},
+    {icon: 'fas fa-igloo', label: 'Amazing Views'},
+    {icon: 'fas fa-laptop-house', label: 'Tiny homes'},
+    {icon: 'fas fa-sink', label: 'Chefs Kitchen'},
+    {icon: 'fas fa-skating', label: 'Surfing'},
   ]
   constructor() { }
 
   ngOnInit(): void {}
+
+  onMouseEnterSidemenu() {
+    const sidemenuEl = document.getElementById('sidemenu');
+    const mainContainerEl = document.getElementById('main-container');
+    sidemenuEl?.classList.remove('col-md-1');
+    sidemenuEl?.classList.add('col-md-2');
+    mainContainerEl?.classList.remove('col-md-11')
+    mainContainerEl?.classList.add('col-md-10')
+  }
+  onMouseLeaveSidemenu(){
+    const sidemenuEl = document.getElementById('sidemenu');
+    const mainContainerEl = document.getElementById('main-container');
+    sidemenuEl?.classList.add('col-md-1');
+    sidemenuEl?.classList.remove('col-md-2');
+    mainContainerEl?.classList.add('col-md-11')
+    mainContainerEl?.classList.remove('col-md-10')
+  }
 
 }
